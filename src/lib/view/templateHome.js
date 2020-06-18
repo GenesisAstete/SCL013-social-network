20219efb9c35060131f6da3c1114e8e03cb3cc2d
GenesisAstete <genesis.astete.a@gmail.com>
2020-06-17 20:51:13 -0400

    boton edicion funcionando solo con una publicacion

--- src/lib/view/templateHome.js
+++ src/lib/view/templateHome.js
@@ -1,24 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { guardar, mostrarPublicacionHome, dataGmail } from '../viewController.js';
-=======
-<<<<<<< HEAD
-import {
-  guardar
-} from "../viewController.js";
-import {
-  editar
-} from "../viewController.js";
-import {
-  mostrarPublicacionHome
-} from "../viewController.js";
->>>>>>> d4ba488... css template login, registro y recuperacion
-
+import { guardar, mostrarPublicacionHome/* , dataGmail */ } from '../viewController.js';
 
 export const home = () => {
   window.location.hash = '/home';
 
-  const user = dataGmail();
+  // const user = dataGmail();
   document.getElementById('root').innerHTML = /* html */ `
   <header>
   <img id="logoMenu" src="./image/logo.jpg">
@@ -54,7 +39,7 @@ export const home = () => {
   </div>
   <div class="contenedorSalida"> 
     <a id="btnHome" href='#/home'></a>
-    <img src="${user.photoURL}" id="btnMuroPersonal" href="#/muroPersonal">
+    <img src="" id="btnMuroPersonal" href="#/muroPersonal">
     <a  id="btnCerrar"></a>
   </div>`;
 
@@ -62,118 +47,5 @@ export const home = () => {
   botonPublicar.addEventListener('click', () => {
     guardar();
   });
-<<<<<<< HEAD
   mostrarPublicacionHome();
 };
-=======
-  mostrarPublicacionHome()
-
-=======
-import {guardar} from "../viewController.js";
-
-import {mostrarPublicacionHome} from "../viewController.js";
-
-export const home = () => {
-
-    window.location.hash = '/home';
-    //leer documentos
-
-    document.getElementById('root').innerHTML =
-        /*html*/ `
-  <header>
-  <img id="logoMenu" src="./image/logo.jpg">
-  <div id="contenedorBotonesMenu">
-      <div class="colorUno">Ruta</div> 
-      <div class="colorDos">Hospedaje</div>
-      <div class="colorUno">Comida</div>
-      <div class="colorDos">Clima</div>
-      <div class="colorUno">Transporte</div>
-      <div class="colorDos">Tour</div>
-  </div>
-  </header>
-  <div id="contenedorEscribir">
-      <input id="inputHome" type="text" placeholder="¿Cual es tu pica'?">
-      <input class="fotoPost" type="file"> 
-      <select id="opcionPublicar">
-
-=======
-import {guardar} from "../viewController.js";
-
-import {mostrarPublicacionHome} from "../viewController.js";
-
-export const home = () => {
-
-    window.location.hash = '/home';
-    //leer documentos
-
-    document.getElementById('root').innerHTML =
-        /*html*/ `
-  <header>
-  <img id="logoMenu" src="./image/logo.jpg">
-  <div id="contenedorBotonesMenu">
-      <div class="colorUno">Ruta</div> 
-      <div class="colorDos">Hospedaje</div>
-      <div class="colorUno">Comida</div>
-      <div class="colorDos">Clima</div>
-      <div class="colorUno">Transporte</div>
-      <div class="colorDos">Tour</div>
-  </div>
-  </header>
-  <div id="contenedorEscribir">
-      <input id="inputHome" type="text" placeholder="¿Cual es tu pica'?">
-      <input class="fotoPost" type="file"> 
-      <select id="opcionPublicar">
-
->>>>>>> 7fd278c... css template login, registro y recuperacion
-      <option>Ruta </option>
-      <option> Hospedaje</option>
-      <option>Comida </option>
-      <option>Clima </option>
-      <option>Transporte </option>
-      <option> Tour</option>
-<<<<<<< HEAD
-
-       </select>
-
- 
-  <button id="btnPublicar"> Publicar</button>
-   </div>
-
-<div id="contenedorMayor">
-
-=======
-
-       </select>
-
- 
-  <button id="btnPublicar"> Publicar</button>
-   </div>
-
-<div id="contenedorMayor">
-
->>>>>>> 7fd278c... css template login, registro y recuperacion
-</div>
-<div class="contenedorSalida"> 
-  <a id="btnHome" href='#/home'></a>
-  <a id="btnMuroPersonal" href="#/muroPersonal"></a>
-  <a  id="btnCerrar"></a>
-</div>
-  `
-
-    const botonPublicar = document.getElementById('btnPublicar');
-    botonPublicar.addEventListener("click", () => {
-        guardar();
-    });
-<<<<<<< HEAD
-<<<<<<< HEAD
-mostrarPublicacionHome()          
-=======
-mostrarPublicacionHome()
->>>>>>> 7fd278c... css template login, registro y recuperacion
-=======
-mostrarPublicacionHome()          
->>>>>>> 433206d... registro guardando usuario
-
->>>>>>> 7fd278c... css template login, registro y recuperacion
-}
->>>>>>> d4ba488... css template login, registro y recuperacion
